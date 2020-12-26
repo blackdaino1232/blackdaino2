eb2ff2750940ffb2b040764b27b1a6b6c737eb60
blackdaino1232 <76617493+blackdaino1232@users.noreply.github.com>
2020-12-26 07:46:38 -0500

    Add files via upload

---
+++ blackdaino.sh
@@ -0,0 +1,77 @@
+#!bin/bash
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((Blackdaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo "((((((((((BlackDaino))))))))))"
+echo ">>>>>>>>>>>>welcome<<<<<<<<<<<"
+echo "wait....."
+sleep 6
+apt-get install update
+apt-get install upgrade
+apt-get install python
+apt-get install python2
+apt-get install python3
+apt-get install git
+apt-get install bash
+apt-get install ruby
+apt-get install php
+apt-get install java
+apt-get install google
+apt-get install perl
+apt-get install nmap
+apt-get install clang
+apt-get install macchanger
+apt-get install nano
+apt-get install figlet
+apt-get install cowsay
+apt-get install curl
+apt-get install tar
+apt-get install zip
+apt-get install unzip
+apt-get install sudo
+apt-get install wget
+apt-get install wcalc
+apt-get install openssl
+apt-get install bmon
+apt-get install golang
+apt-get install host
+apt-get install havij
+apt-get install hydra
+apt-get install wireshark
+apt-get install cmatrix
+apt-get install wget -y
+apt-get install python -y
+apt-get install python2 -y
+apt-get install python3 -y
+apt-get install python-dev -y
+apt-get install toilet
+apt-get install help
+apt-get install lolcat
+apt-get install wgetrc
+apt-get install tor
+apt-get install net-tools
+apt-get install unrar
+apt-get install w3m
+apt-get install proof
+apt-get install pip2 install requests
+apt dist-upgrade -y
+apt-get install c
+apt-get install c++
+apt-get install c#
+apt-get install man
+apt-get install metasploit
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+echo "THANK YOU >>>>>>> BLACKDAINO"
+
